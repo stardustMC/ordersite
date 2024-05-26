a357bb9160be026f9954d629133042d4256f5e94
曹汝琛 <caoruchen@stu.ouc.edu.cn>
2024-05-26 16:42:07 +0800

    script fixed2.0

--- script/init_others.py
+++ script/init_others.py
@@ -13,6 +13,21 @@ django.setup()
 from web import models
 from utils.encryption import md5_encrypt
 
+# initialize prior
+prior_count = 5
+for i in range(prior_count):
+    models.Prior.objects.create(
+        title="会员{}".format(i),
+        discount=100 - i * prior_count,
+        active=1,
+    )
+
+# initialize price policy
+models.PricePolicy.objects.create(count=1000, price=20)
+models.PricePolicy.objects.create(count=2000, price=35)
+models.PricePolicy.objects.create(count=5000, price=80)
+
+# initialize customer
 models.Customer.objects.create(
         active=1,
         username='caoruchen',
@@ -32,6 +47,6 @@ for i in range(50):
         phone='1' + str(randint(1000000000, 9999999999)),
         email=str(randint(1000000, 9999999)) + '@' + ['qq.com', '163.com', 'gmail.com', '126.com'][randint(0, 3)],
         balance=5 * randint(20, 100),
-        prior_id=randint(1, 3),
+        prior_id=randint(1, prior_count),
         creator_id=1,
     )
